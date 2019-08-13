80eb5393f48e49e4c1257eeed03904ea8728b546
suzutatsu <suzuta1221@gmail.com>
2019-08-13 17:16:06 +0900

    [update]4-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+get '/' => 'root#top'
+get 'index' => 'book#index'
+post 'book' => 'book#create'
+
 end
+
+
